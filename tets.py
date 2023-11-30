1826beb48566244f5ea5d8e619fc0dbae1847635
AlexBeszedin <alebesedin@yandex.ru>
2023-11-30 15:54:49 +0300

    fix structure

--- tets.py
+++
@@ -1,9 +0,0 @@
-response = requests.get(url)
-
-if response.status_code == 200:
-        data = response.json()
-        print(f'Network:', data['result']['node_info']['network'])
-        print(f'version:', data['result']['node_info']['version'])
-        print(f'latest_block_height:', data['result']['sync_info']['latest_block_height'])
-        print(f'latest_block_time:', data['result']['sync_info']['latest_block_time'])
-        
